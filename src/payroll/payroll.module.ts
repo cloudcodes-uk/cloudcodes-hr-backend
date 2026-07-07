3d7b3e78ce3315052059339dd0fc12d223690d13
Mostafa Elsaeed <98340604+Mostafa-Elsaeed@users.noreply.github.com>
2026-07-07 15:36:23 +0300

    fix: import EmployeeModule in PayrollModule so Nest can resolve EmployeeProfileRepository"

--- src/payroll/payroll.module.ts
+++ src/payroll/payroll.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { EmployeeModule } from '../employees/employees.module';
+import { LeaveModule } from '../leave/leave.module';
 import { PayrollController } from './payroll.controller';
-import { EmployeesService } from '../employees/employees.service';
-import { LeaveService } from '../leave/leave.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([])],
+  imports: [EmployeeModule, LeaveModule],
   controllers: [PayrollController],
-  providers: [EmployeesService, LeaveService],
 })
 export class PayrollModule {}
